86599004c0c7fe65302dd574d8c420bf66b89311
Takafumi Suzuki <highdro0479@gmail.com>
2020-01-04 23:49:00 +0900

    add: models

--- chiba_public/models.py
+++ chiba_public/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Facility(models.Model):
+    name = models.CharField(max_length=60, null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name_kana = models.CharField(max_length=128)
+    address = models.CharField(max_length=60)
+    latitude = models.FloatField(null=False)
+    longitude = models.FloatField(null=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Facility'
+        verbose_name_plural = 'Facilities'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, null=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
